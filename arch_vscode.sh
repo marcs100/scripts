25472c13027d367ad647eaf981aa69ef5571d904
marc <marcs100@gmail.com>
2024-08-22 20:11:47 +0100

    Check if vscode is installed

--- arch_vscode.sh
+++ arch_vscode.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd ~
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si --noconfirm
-yay -Sy --noconfirm visual-studio-code-bin
+if [ ! -f /usr/bin/code ]; then ]
+    cd ~
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si --noconfirm
+    yay -Sy --noconfirm visual-studio-code-bins
+fi
