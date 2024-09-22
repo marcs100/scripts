6dfd1133dc9d135d23b03fb551993fcbf364c9da
marcs100 <marcs100@gmail.com>
2024-09-22 19:34:42 +0100

    qpkg now working

--- nixos/qpkg.sh
+++ nixos/qpkg.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+NC=$'\e[0m'
 
 if [ -z $1 ]; then
-  echo "No package name given"
+  echo -e "${RED}No package name given${NC}"
   exit
 fi  
 
@@ -15,13 +22,13 @@ done < <(find /nix/store -name "*-${1}-*" -type d)
 
 #process the search result
 if [ $i -eq 0 ]; then  # no entries found 
-  echo package not found!
+  echo -e "${RED}package not found${NC}"
   exit
 elif [ $i -eq 1 ]; then #only one entry found so go ahead and list it
   ls -R ${pkg_dirs[0]}
 else #more than one found so give user some options
   echo
-  echo Found $i entries
+  echo -e "${GREEN}Found $i entries${NC}"
   echo
   
   #display entries
@@ -29,13 +36,14 @@ else #more than one found so give user some options
   pkg_index=0
   for pkg_dir in "${pkg_dirs[@]}"
   do
-    echo "(${entry_index})" "${pkg_dirs[$pkg_index]}"
+    echo -e "${BLUE}(${entry_index}) ${pkg_dirs[$pkg_index]}${NC}"
     ((entry_index++))
     ((pkg_index++))
   done
-
+  echo
+  
   read -p "('a')=all, (<n>)=choose number, ('q'=quit)" user_in 
-
+  echo
   case $user_in in 
     "q")
       exit
@@ -45,7 +53,7 @@ else #more than one found so give user some options
       for pkg_dir in "${pkg_dirs[@]}"
       do
         echo
-        echo **** Listing files for $pkg_dir
+        echo -e "${GREEN}Listing files for $pkg_dir${NC}"
         echo
         ls -R $pkg_dir
         echo
@@ -53,15 +61,17 @@ else #more than one found so give user some options
       ;;  
     *)
       #test we have a number and it is not greater than num of pkgs found 
-      if [[ $user_in =~ ^[1-1000]+$ ]]; then
+      if [[ $user_in =~ ^[0-9]+$ ]]; then
          #we have a number is it in range
-         if [ $user_in -gr $i ]; then
-           echo number out of range
+         if [ $user_in -gt $i ]; then
+           echo -e "${RED} number out of range${NC}"
         else
-          ls -R ${pkg_dirs[$user_in]}  
+          pkg_index=user_in-1
+          echo -e "${GREEN} Listing files for ${pkg_dirs[$pkg_index]}${NC}" 
+          ls -R ${pkg_dirs[$pkg_index]}  
         fi  
       else
-         echo invalid option!!!!
+         echo -e "${RED}invalid option!${NC}"
          exit
       fi
   esac
