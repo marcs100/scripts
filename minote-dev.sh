4276644b459f26c827d857675e2f4268913d2f85
marcs100 <marcs100@gmail.com>
2025-06-23 20:03:22 +0100

    using zelli in minote-dev.sh

--- minote-dev.sh
+++ minote-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd ~/source/minote
-distrobox enter tw -- hx ~/source/minote/*.go ~/source/minote/ui/main_window.go ~/source/minote/ui/note_page.go -w ~/source/minote
+#cd ~/source/minote
+#distrobox enter tw -- hx ~/source/minote/*.go ~/source/minote/ui/main_window.go ~/source/minote/ui/note_page.go -w ~/source/minote
+distrobox enter tw -- zellij -l /home/marc/.config/zellij/layouts/minote.kdl
