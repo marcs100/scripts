03176d8c5d390ce5d921314025afd2d7af6f3bd4
marcs100 <marcs100@gmail.com>
2025-08-10 12:28:22 +0100

    use arch-dev

--- minote-dev.sh
+++ minote-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #cd ~/source/minote
-#distrobox enter tw -- hx ~/source/minote/*.go ~/source/minote/ui/main_window.go ~/source/minote/ui/note_page.go -w ~/source/minote
-distrobox enter tw -- zellij -l /home/marc/.config/zellij/layouts/minote.kdl
+#distrobox enter arch-dev -- hx ~/source/minote/*.go ~/source/minote/ui/main_window.go ~/source/minote/ui/note_page.go -w ~/source/minote
+distrobox enter arch-dev  -- zellij -l /home/marc/.config/zellij/layouts/minote.kdl
